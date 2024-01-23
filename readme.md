2e544d5ff9ae7015965d1321263169b6397fd30e
Antonio Macena <75036720+AntonMac@users.noreply.github.com>
2024-01-23 17:28:42 -0300

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,16 @@
+<h1 align="center">Sprint 01: Construa um decodificador de texto com Javascript</h1>
+ By Antonio Macena
+
+
+# Desafio
+Challenge 01 do programa ONE - Oracle Next Education criação de um encriptador/desencriptador de textos.
+
+## Chave da criptografia:
+Originalmente pretendia utilizar a chave importando de um arquivo js. com um array no formato [ {key: _key, value: _value}], mas algum bug está impedindo o import de funcionar, então passei a chave diretamente no js princinpal. Não é uma chava muito complexa, pois apenas muda a posição dos caracteres, mas com o tempo pretendo aprimorar ou utilizar alguma API especifica pra encriptação.
+
+## Visual:
+Não tentei imitar o modelo disponibilizado pois ainda não domino corretamente CCS a ponto de implementar corretamente o modelo, preferi então por um thema escuro que facilitasse a visualização com uma estrutura simetrica.
+
+
+## Tecnologias:
+- HTML, CSS e JavaScript
---
+++ script.js
@@ -0,0 +1,149 @@
+// Antonio Macena
+
+//TODO - import do modulo está freezando o script
+/*
+import  { _chave }  from './chave.js';
+*/
+
+let _chave = [
+    { key: '0', value: 'A' },
+    { key: '1', value: 'C' },
+    { key: '2', value: 'E' },
+    { key: '3', value: 'f' },
+    { key: '4', value: 'R' },
+    { key: '5', value: 'g' },
+    { key: '6', value: 'k' },
+    { key: '7', value: 'l' },
+    { key: '8', value: 'i' },
+    { key: '9', value: 'L' },
+    { key: 'A', value: 'r' },
+    { key: 'B', value: 'o' },
+    { key: 'C', value: 'M' },
+    { key: 'D', value: 'b' },
+    { key: 'E', value: 'h' },
+    { key: 'F', value: 'D' },
+    { key: 'G', value: 'p' },
+    { key: 'H', value: 'q' },
+    { key: 'I', value: 'v' },
+    { key: 'J', value: '1' },
+    { key: 'K', value: 'S' },
+    { key: 'L', value: 'O' },
+    { key: 'M', value: 't' },
+    { key: 'N', value: 'j' },
+    { key: 'O', value: '7' },
+    { key: 'P', value: 'a' },
+    { key: 'Q', value: '4' },
+    { key: 'R', value: 'F' },
+    { key: 'S', value: 'U' },
+    { key: 'T', value: '5' },
+    { key: 'U', value: 'I' },
+    { key: 'V', value: 'z' },
+    { key: 'W', value: 'u' },
+    { key: 'X', value: '?' },
+    { key: 'Y', value: 'n' },
+    { key: 'Z', value: '2' },
+    { key: 'a', value: 'Ç' },
+    { key: 'b', value: 'V' },
+    { key: 'c', value: '0' },
+    { key: 'd', value: 'Q' },
+    { key: 'e', value: '9' },
+    { key: 'f', value: 'z' },
+    { key: 'g', value: 'H' },
+    { key: 'h', value: '6' },
+    { key: 'i', value: 'x' },
+    { key: 'j', value: 'w' },
+    { key: 'k', value: 'ç' },
+    { key: 'l', value: 'e' },
+    { key: 'm', value: 'J' },
+    { key: 'n', value: '3' },
+    { key: 'o', value: 'N' },
+    { key: 'p', value: 'W' },
+    { key: 'q', value: 'd' },
+    { key: 'r', value: 'K' },
+    { key: 's', value: 'X' },
+    { key: 't', value: 'c' },
+    { key: 'u', value: 'y' },
+    { key: 'v', value: 's' },
+    { key: 'w', value: 'B' },
+    { key: 'x', value: 'm' },
+    { key: 'y', value: 'P' },
+    { key: 'z', value: '8' },
+    { key: 'ç', value: 'T' },
+    { key: 'Ç', value: 'Y' },
+    { key: '?', value: 'G' }
+
+];
+
+
+function cifrar(strEntrada) {
+    let strSaida ="";
+    
+    for (let c of strEntrada){
+        let foradaChave = false;
+        _chave.forEach( x => {
+            if(c==x.key){
+                strSaida = strSaida + x.value;
+                foradaChave = true;
+            }
+        });  
+        if(!foradaChave) {
+            strSaida = strSaida + c;
+        }
+    }
+    return strSaida;
+
+}
+
+function deCifrar(strEntrada) {
+    let strSaida ="";
+    for (let c of strEntrada){
+        let foradaChave = false;
+        _chave.forEach( x => {
+            if(c==x.value){
+                strSaida = strSaida + x.key;
+                foradaChave = true;
+            }
+        });
+        if(!foradaChave) {
+            strSaida = strSaida + c;
+        }
+    }
+    return strSaida;
+
+}
+
+function codificar() {
+    let inputText = document.getElementById("inputBox").value;
+    if (inputText.trim() === "") {
+        alert("Nenhum Texto a ser Codificado, Digite seu texto!");
+        return;
+    }
+    let resultBox = document.getElementById("resultBox");
+    resultBox.textContent = cifrar(inputText.trim());
+}
+
+function deCodificar() {
+    let inputText = document.getElementById("inputBox").value;
+    if (inputText.trim() === "") {
+        alert("Nenhum Texto a ser Decodificado, Digite seu texto!");
+        return;
+    }
+    let resultBox = document.getElementById("resultBox");
+    resultBox.textContent = deCifrar(inputText.trim());
+}
+
+function copiar() {
+    let resultBox = document.getElementById("resultBox");
+    let copiaTexto = resultBox.textContent;
+    if (copiaTexto.trim() === "") {
+        alert("Não há texto a ser copiado!");
+        return;
+    }
+    let tempTextarea = document.createElement("textarea");
+    tempTextarea.value = copiaTexto;
+
+    document.body.appendChild(tempTextarea);
+    tempTextarea.select();
+    document.body.removeChild(tempTextarea);
+}
+
